6f106ca5a5289ba1d72f85eaa0df619fc339ce03
Martin Malmsten <martin.malmsten@kb.se>
2021-01-15 11:11:56 +0100

    C

--- utils.py
+++ utils.py
@@ -8,8 +8,10 @@ def tag_content(content, tags):
         #end_tag = '</span>'
         #start_tag = f'<div class="ui left labeled button"><a class="ui basic label">'
         #end_tag = f'</a><div class="ui icon button">{t.type.name}</div></div>'
-        start_tag = f'<div class="ui label" style="background: {t.type.color}7f">'
-        end_tag = f'<a class="detail">{t.type.name}</a><i class="delete icon"></i></div>'    
+        #start_tag = f'<div class="ui label" style="background: {t.type.color}7f">'
+        #end_tag = f'<a class="detail">{t.type.name}</a><i class="delete icon"></i></div>'
+        start_tag = f'<span class="tag">'
+        end_tag = f'</span>'
 
         content = content[:t.start + offset] + \
                   start_tag + \
